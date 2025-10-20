8329fb05ea9f6f2830ab8b765655e96f2ae325be
Prajwal Chigod <chigod.devs@protonmail.com>
2025-10-20 20:24:45 +0530

    Refactored blink completions to use defaults and optimization for max performance

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -5,53 +5,35 @@ return {
 		version = "v0.*",
 		opts = {
 			keymap = {
-				preset = "default",
 				["<Tab>"] = { "accept", "snippet_forward", "fallback" },
 				["<S-Tab>"] = { "snippet_backward", "fallback" },
 			},
 			appearance = {
 				use_nvim_cmp_as_default = true,
-				nerd_font_variant = "mono",
 			},
 			sources = {
-				default = { "lsp", "path", "buffer" },
 				providers = {
 					buffer = {
 						min_keyword_length = 4,
 						max_items = 5,
+						score_offset = -3,
 					},
 					lsp = {
 						max_items = 15,
+						score_offset = 10,
+					},
+					path = {
+						score_offset = 5,
 					},
 				},
-			},
-			cmdline = {
-				enabled = false,
 			},
 			completion = {
-				trigger = {
-					show_on_keyword = true,
-				},
 				menu = {
 					border = "rounded",
-					auto_show = true,
-				},
-				list = {
-					selection = {
-						preselect = true,
-						auto_insert = true,
-					},
-				},
-				accept = {
-					auto_brackets = {
-						enabled = true,
-					},
 				},
 				documentation = {
 					auto_show = false,
-				},
-				ghost_text = {
-					enabled = false,
+					auto_show_delay_ms = 500,
 				},
 			},
 		},
