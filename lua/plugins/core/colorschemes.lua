8fa3650a3747151e4bbe4e52da84e690e3e14114
Prajwal Chigod <chigod.devs@protonmail.com>
2026-02-04 14:02:01 +0530

    Removed unused colorschemes

--- lua/plugins/core/colorschemes.lua
+++ lua/plugins/core/colorschemes.lua
@@ -7,7 +7,7 @@ return {
 		config = function()
 			require("catppuccin").setup({
 				flavour = "macchiato",
-        term_colors = true,
+				term_colors = true,
 				compile = {
 					enabled = true,
 					path = vim.fn.stdpath("cache") .. "/catppuccin",
@@ -24,21 +24,6 @@ return {
 					comments = { "italic" },
 					conditionals = { "italic" },
 				},
-				color_overrides = {
-					macchiato = {
-						base = "#000000",
-						mantle = "#181825",
-						crust = "#11111b",
-					},
-				},
-				custom_highlights = function(colors)
-					return {
-						String = { fg = colors.teal },
-						["@string"] = { fg = colors.teal },
-						Comment = { fg = colors.overlay0, style = { "italic" } },
-						["@comment"] = { fg = colors.overlay0, style = { "italic" } },
-					}
-				end,
 				integrations = {
 					cmp = false,
 					mini = {
@@ -49,7 +34,7 @@ return {
 		end,
 	},
 	{
-		"vague2k/vague.nvim",
+		"rebelot/kanagawa.nvim",
 		lazy = true,
 		opts = {},
 	},
