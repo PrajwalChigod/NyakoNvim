fa1f3e21a56187a095780dabc2644cec79c9b8d8
Prajwal Chigod <chigod.devs@protonmail.com>
2026-05-01 21:12:37 +0530

    added diffview plugin

---
+++ lua/plugins/core/diffview.lua
@@ -0,0 +1,53 @@
+return {
+	{
+		"sindrets/diffview.nvim",
+		cmd = {
+			"DiffviewOpen",
+			"DiffviewClose",
+			"DiffviewToggleFiles",
+			"DiffviewFocusFiles",
+			"DiffviewFileHistory",
+		},
+		keys = {
+			{ "<leader>gm", "<cmd>DiffviewOpen --merge-tool<CR>", desc = "Git: Open 3-pane merge tool" },
+			{ "<leader>gM", "<cmd>DiffviewClose<CR>",             desc = "Git: Close diffview" },
+			{ "<leader>gh", "<cmd>DiffviewFileHistory %<CR>",     desc = "Git: File history" },
+		},
+		config = function()
+			local actions = require("diffview.actions")
+
+			require("diffview").setup({
+				view = {
+					merge_tool = {
+						-- LOCAL | BASE | REMOTE on top, MERGED on bottom
+						layout = "diff4_mixed",
+						disable_diagnostics = true,
+					},
+				},
+				keymaps = {
+					disable_defaults = false,
+					-- Active in all diff view panels (LOCAL / BASE / REMOTE panels)
+					view = {
+						{ "n", "q",          "<cmd>DiffviewClose<CR>", { desc = "Close diffview" } },
+						{ "n", "<leader>gM", "<cmd>DiffviewClose<CR>", { desc = "Close diffview" } },
+					},
+					-- Active in the MERGED (bottom) panel of the 4-way merge editor
+					diff4 = {
+						{ "n", "co",  actions.diffget("ours"),   { desc = "Merge: take LOCAL (ours)" } },
+						{ "n", "ct",  actions.diffget("theirs"), { desc = "Merge: take REMOTE (theirs)" } },
+						{ "n", "cb",  actions.diffget("base"),   { desc = "Merge: take BASE" } },
+						{ "n", "]x",  actions.next_conflict,     { desc = "Next conflict" } },
+						{ "n", "[x",  actions.prev_conflict,     { desc = "Prev conflict" } },
+						{ "n", "q",   "<cmd>DiffviewClose<CR>",  { desc = "Close diffview" } },
+					},
+					file_panel = {
+						{ "n", "q", "<cmd>DiffviewClose<CR>", { desc = "Close diffview" } },
+					},
+					file_history_panel = {
+						{ "n", "q", "<cmd>DiffviewClose<CR>", { desc = "Close diffview" } },
+					},
+				},
+			})
+		end,
+	},
+}
